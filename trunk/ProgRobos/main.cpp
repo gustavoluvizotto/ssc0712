40a8da6bf7ff0b165855e6612fb354abd3debbf2
gustavoluvizotto@gmail.com <gustavoluvizotto@gmail.com@8996064f-9593-6cd5-d6aa-b59c90998e7b>
2013-05-17 17:51:36 +0000

    iniciando o projeto =)

---
+++ trunk/ProgRobos/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Gustavo Luvizotto Cesar      - 6783544
+ *         Leonardo Lourenço Crespilho  - 5124310 
+ * Created on May 17, 2013, 2:46 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
