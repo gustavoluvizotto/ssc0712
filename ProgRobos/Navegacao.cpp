8569e252d7171e1fbb69e112926bf28e1dfe71ab
gustavoluvizotto@gmail.com <gustavoluvizotto@gmail.com@8996064f-9593-6cd5-d6aa-b59c90998e7b>
2013-05-24 23:09:55 +0000

    zica da malasia

--- ProgRobos/Navegacao.cpp
+++ ProgRobos/Navegacao.cpp
@@ -1,7 +1,7 @@
 /* 
  * File:   Navegacao.cpp
- * Author: gustavo
- * 
+ * Author: Gustavo Luvizotto Cesar      - 6783544
+ *         Leonardo Lourenço Crespilho  - 5124310 
  * Created on May 24, 2013, 6:09 PM
  */
 
--- ProgRobos/Robo.h
+++ ProgRobos/Robo.h
@@ -1,7 +1,7 @@
 /* 
  * File:   Robo.h
- * Author: gustavo
- *
+ * Author: Gustavo Luvizotto Cesar      - 6783544
+ *         Leonardo Lourenço Crespilho  - 5124310 
  * Created on May 24, 2013, 6:02 PM
  */
 
